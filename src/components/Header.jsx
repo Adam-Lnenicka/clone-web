67eae0b39964ec3837c2bd01c870f8818bf070f2
Adam-Lnenicka <70516378+Adam-Lnenicka@users.noreply.github.com>
2021-01-26 12:28:01 +0000

    no warnings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header(){
 
     <div className="Header">
     <nav class="navbar1 navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand navbar-brand3" href="/"><img src={Logo}/></a>
+    <a class="navbar-brand navbar-brand3" href="/"><img src={Logo} alt="logo"/></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span > <i class="fa fa-bars"></i><div>Menu</div></span>
     </button>
@@ -65,8 +65,7 @@ export default function Header(){
     </div>
     
   </nav>
-  <p style={{color:"red"}}>Clone website created for job application purposes. <br></br>
-    Pages recreated: Home, About, Department of Dreams</p>
+  <p style={{color:"red"}}>Clone website created for job application purposes. <br></br></p>
   
   </div>
   
